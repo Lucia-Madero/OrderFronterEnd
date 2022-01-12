a88ad10be54562fcf5a8b4a4f76ac882321e21d0
Lucia Madero <maderolucia@gmail.com>
2022-01-12 14:27:22 +0100

    Item overview - added name filter pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import {AppRoutingModule} from './app-routing.module';
 import {LayoutModule} from "./layout/layout.module";
 import {HttpClientModule} from "@angular/common/http";
 import { ItemOverviewComponent } from './item-overview/item-overview.component';
+import { NameFilterPipe } from './pipes/name-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemOverviewComponent,
+    NameFilterPipe,
   ],
   imports: [
     BrowserModule,
