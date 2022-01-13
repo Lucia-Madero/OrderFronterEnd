614ed381821efdd1ea942d277db56eee925d2902
Lucia Madero <maderolucia@gmail.com>
2022-01-13 16:08:43 +0100

    Update item - created update item form component

---
+++ src/app/update-item-form/update-item-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UpdateItemFormComponent } from './update-item-form.component';
+
+describe('UpdateItemFormComponent', () => {
+  let component: UpdateItemFormComponent;
+  let fixture: ComponentFixture<UpdateItemFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UpdateItemFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UpdateItemFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/update-item-form/update-item-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-item-form',
+  templateUrl: './update-item-form.component.html',
+  styleUrls: ['./update-item-form.component.css']
+})
+export class UpdateItemFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
