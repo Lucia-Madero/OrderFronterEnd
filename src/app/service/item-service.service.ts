25f3df4dda8eb1e87f149d6dbab43302196bf589
Lucia Madero <maderolucia@gmail.com>
2022-01-14 17:23:58 +0100

    Refactoring - cleaned up item service

--- src/app/service/item-service.service.ts
+++ src/app/service/item-service.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Item} from "../model/item";
-import {catchError, Observable, throwError} from "rxjs";
+import {Observable} from "rxjs";
 import {Router} from "@angular/router";
 
 @Injectable({
@@ -14,7 +14,7 @@ export class ItemServiceService {
   httpOptions = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
 
 
-  constructor(private httpClient: HttpClient, private router: Router) {
+  constructor(private httpClient: HttpClient) {
     this.itemsUrl = `${environment.backendUrl}/items`
 
   }
@@ -25,8 +25,6 @@ export class ItemServiceService {
 
   addItem(item: Item): Observable<Item> {
     return this.httpClient.post<Item>(this.itemsUrl, item);
-
-
   }
 
   findById(id: string): Observable<Item> {
@@ -34,13 +32,6 @@ export class ItemServiceService {
   }
 
   updateItem(id: string, value: any): Observable<void> {
-    const existingItem = this.findById(id)
-      .pipe(
-        catchError(err => {
-          this.router.navigate(['items']);
-          return throwError(err);
-        })
-      )
     return this.httpClient.put<void>(`${this.itemsUrl}/${id}`, value);
   }
 
